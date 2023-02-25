057cb37390ee61a4b63edc06a0a31255d80ca415
praneeth1818 <113609605+praneeth1818@users.noreply.github.com>
2023-02-25 11:28:58 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,5 @@
+two_thirds_income = 480
+total_income =two_thirds_income * 3/2
+one_fourth_income = total_income * 1/4
+print("Sam's weekly income is:", total_income)
+print("One fourth of Sam's weekly income is:", one_fourth_income)
